0398aa583839038aec68d622b3cc8fa86b22fe34
Anthony Maudry <anthony.maudry@thuata.com>
2016-11-23 00:32:50 +0100

    started doc and added shortcuts to factories

--- Service/ServiceFactory.php
+++ Service/ServiceFactory.php
@@ -26,6 +26,8 @@
 
 namespace Thuata\FrameworkBundle\Service;
 
+use Thuata\FrameworkBundle\Exception\InvalidServiceException;
+use Thuata\FrameworkBundle\Exception\ServiceNotFoundException;
 use Thuata\FrameworkBundle\Factory\AbstractFactory;
 use Thuata\FrameworkBundle\Factory\Factorable\FactorableInterface;
 use Thuata\ComponentBundle\Registry\RegistryableTrait;
@@ -42,42 +44,79 @@ class ServiceFactory extends AbstractFactory
 {
     use RegistryableTrait;
 
+    private $serviceIdMap = [];
+
     /**
      * {@inheritdoc}
      */
     protected function injectDependancies(FactorableInterface $factorable)
     {
+        if (!($factorable instanceof AbstractService)) {
+            throw new InvalidServiceException(get_class($factorable));
+        }
+
         /** @var AbstractService $factorable */
         $managerFactory = $this->getContainer()->get('thuata_framework.managerfactory');
 
         $factorable->setManagerFactory($managerFactory);
     }
-    
+
     /**
      * Called when factorable is loaded
-     * 
+     *
      * @param FactorableInterface $factorable
      */
     protected function onFactorableLoaded(FactorableInterface $factorable)
     {
         $this->addToRegistry($factorable);
     }
-    
+
     /**
      * Gets a service
-     * 
+     *
      * @param string $factorableClassName
-     * 
+     *
      * @return AbstractService
      */
     public function getFactorableInstance(string $factorableClassName)
     {
         $factorable = $this->loadFromRegistry($factorableClassName);
-        
+
         if (!$factorable instanceof AbstractService) {
             $factorable = parent::getFactorableInstance($factorableClassName);
         }
-        
+
         return $factorable;
+    }
+
+    /**
+     * Get a service by its class name or symfony service id
+     * do the same as getFactorableInstance method, was created for convenience
+     *
+     * @param string $service
+     *
+     * @return \Thuata\FrameworkBundle\Service\AbstractService
+     */
+    public function getService(string $service): AbstractService
+    {
+        return $this->getFactorableInstance($service);
+    }
+
+    /**
+     * @param string $factorableClassName
+     *
+     * @return \Thuata\FrameworkBundle\Factory\Factorable\FactorableInterface
+     */
+    protected function instanciateFactorable(string $factorableClassName)
+    {
+        if (class_exists($factorableClassName)) {
+            $service = parent::instanciateFactorable($factorableClassName);
+        } elseif ($this->getContainer()->has($factorableClassName)) {
+            $service = $this->getContainer()->get($factorableClassName);
+        } else {
+            throw new ServiceNotFoundException($factorableClassName);
+        }
+
+        return $service;
     }
 }
